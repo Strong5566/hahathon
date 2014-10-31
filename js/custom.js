aa96985367179c22e48d28ca79e6fd3127f65b61
Strong <strongerh9@gmail.com>
2014-11-01 01:38:44 +0800

    no message

---
+++ js/custom.js
@@ -0,0 +1,38 @@
+$('#sign-btn').click(function(){
+
+
+	alert("抱歉，11/7才開放報名唷~");
+});
+
+$('.nav>li').click(function(){
+	var left = $(this).position().left + $(this).width()/2 - 51
+	var theta = left - parseInt($('.rotation').css('margin-left'))
+	$('.rotation').animate({
+		'margin-left' : left,
+		'margin-right' : -50-left
+	
+	},
+	{
+		duration: Math.abs(theta)*3
+	});
+	$('.rotation').animateRotate(theta, Math.abs(theta)*3);
+});
+
+$.fn.animateRotate = function(angle, duration, easing, complete) {
+  var args = $.speed(duration, easing, complete);
+  var step = args.step;
+  return this.each(function(i, e) {
+    args.complete = $.proxy(args.complete, e);
+    args.step = function(now) {
+      $.style(e, 'transform', 'rotate(' + now + 'deg)');
+      if (step) return step.apply(e, arguments);
+    };
+
+    $({deg: 0}).animate({deg: angle}, args);
+  });
+};
+
+
+/*$('.dropdown').hover(function(){ 
+  $('.dropdown-toggle', this).trigger('click'); 
+});*/
---
+++ js/scrolling-nav.js
@@ -0,0 +1,32 @@
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+	
+
+	
+});
+
+
+/*$(function(){
+	var y = $(document).scrollTop();
+    var t = $(".about-section").offset().top;
+    if (y > t) {
+		$(".about-section").css('background', 'url(../img/p3-09.jpg) no-repeat center center scroll');
+    } else {
+        $(".about-section").css('background', 'transparent');
+    }
+});*/
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
